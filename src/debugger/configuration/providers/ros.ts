efef47e8a5bdc890f97cae8136877ce93f02cc7d
James Xu <kejxu@microsoft.com>
2019-08-06 11:40:20 -0700

    revert cleanup, add comment

--- src/debugger/configuration/providers/ros.ts
+++ src/debugger/configuration/providers/ros.ts
@@ -1,14 +1,15 @@
 // Copyright (c) Andrew Short. All rights reserved.
 // Licensed under the MIT License.
 
-/* tslint:disable */
-
 import * as vscode from "vscode";
 
 // interact with the user to create a roslaunch or rosrun configuration
 export class RosDebugConfigurationProvider implements vscode.DebugConfigurationProvider {
     public async provideDebugConfigurations(folder: vscode.WorkspaceFolder | undefined, token?: vscode.CancellationToken): Promise<vscode.DebugConfiguration[]> {
         const configs: vscode.DebugConfiguration[] = undefined;
+
+        // this could be implemented to provide debug configurations interactively
+        // generate configurations with snippets defined in package.json for now
         return configs;
     }
 }
