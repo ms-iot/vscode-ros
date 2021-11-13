93b12075c7a54a7b9e242a1e9ec738200824410a
Lou Amadio <lamadio@microsoft.com>
2021-11-13 10:22:19 -0800

    webpack builds

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,31 @@ const config = {
       // Webpack 5 no longer polyfills Node.js core modules automatically.
       // see https://webpack.js.org/configuration/resolve/#resolvefallback
       // for the list of Node.js core module polyfills.
-      "path": require.resolve("path-browserify")
+      assert: require.resolve('assert'),
+      buffer: require.resolve('buffer'),
+      console: require.resolve('console-browserify'),
+      constants: require.resolve('constants-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      domain: require.resolve('domain-browser'),
+      events: require.resolve('events'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      path: require.resolve('path-browserify'),
+      punycode: require.resolve('punycode'),
+      querystring: require.resolve('querystring-es3'),
+      stream: require.resolve('stream-browserify'),
+      string_decoder: require.resolve('string_decoder'),
+      sys: require.resolve('util'),
+      timers: require.resolve('timers-browserify'),
+      url: require.resolve('url'),
+      util: require.resolve('util'),
+      fs: false,
+      child_process: false,
+      os: false,
+      process: false,
+      readline: false,
+      net: false
+
     }
   },
   module: {
