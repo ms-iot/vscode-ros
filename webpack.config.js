1b9032c3c50c0ec53ca14fcb2183ebca4d7de72a
Lou Amadio <lamadio@microsoft.com>
2022-01-31 14:53:30 -0800

    Update version

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,10 @@
 'use strict';
 
 const path = require('path');
-const webpack = require('webpack');
 
 /**@type {import('webpack').Configuration}*/
 const config = {
-  target: 'webworker', // vscode extensions run in webworker context for VS Code web 📖 -> https://webpack.js.org/configuration/target/#target
+  target: 'node',
 
   entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
@@ -22,55 +21,27 @@ const config = {
     vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
   },
   resolve: {
-    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
     extensions: ['.ts', '.js'],
-    alias: {
-      // provides alternate implementation for node module and source files
-    },
+//    preferRelative: true,
     fallback: {
       // Webpack 5 no longer polyfills Node.js core modules automatically.
       // see https://webpack.js.org/configuration/resolve/#resolvefallback
       // for the list of Node.js core module polyfills.
-      assert: require.resolve('assert'),
-      buffer: require.resolve('buffer'),
-      console: require.resolve('console-browserify'),
-      constants: require.resolve('constants-browserify'),
-      crypto: require.resolve('crypto-browserify'),
-      domain: require.resolve('domain-browser'),
-      events: require.resolve('events'),
-      http: require.resolve('stream-http'),
-      https: require.resolve('https-browserify'),
-      path: require.resolve('path-browserify'),
-      punycode: require.resolve('punycode'),
-      querystring: require.resolve('querystring-es3'),
-      stream: require.resolve('stream-browserify'),
-      string_decoder: require.resolve('string_decoder'),
-      sys: require.resolve('util'),
-      timers: require.resolve('timers-browserify'),
-      url: require.resolve('url'),
-      util: require.resolve('util'),
-      fs: false,
-      child_process: false,
-      os: false,
-      process: false,
-      readline: false,
-      net: false
-
-    }
-  },
-  module: {
-    rules: [
-      {
+    }  },
+    module: {
+      rules: [{
         test: /\.ts$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader'
-          }
-        ]
-      }
-    ]
-  }
+        use: [{
+            loader: 'ts-loader',
+            options: {
+                compilerOptions: {
+                    "module": "es6" // override `tsconfig.json` so that TypeScript emits native JavaScript modules.
+                }
+            }
+        }]
+      }]
+    },
 };
+
 module.exports = config;
