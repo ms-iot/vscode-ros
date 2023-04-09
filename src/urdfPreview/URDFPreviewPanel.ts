a5e4b8b1145248e6a0b5527d0224e2724d42c00f
Lou Amadio <lou@polyhobbyist.com>
2023-04-09 10:47:02 -0700

    Bootstrap new URDF Previewer

---
+++ src/urdfPreview/URDFPreviewPanel.ts
@@ -0,0 +1,128 @@
+import * as BABYLON from 'babylonjs';
+import * as Materials from 'babylonjs-materials';
+import * as urdf from 'babylon_ros';
+
+// Get access to the VS Code API from within the webview context
+const vscode = acquireVsCodeApi();
+
+const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement; // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+var scene = new BABYLON.Scene(engine);
+
+function applyAxisToTransform(scene : BABYLON.Scene, t : BABYLON.TransformNode | undefined) {
+  if (t) {
+    let a = new BABYLON.AxesViewer(scene, .5);
+    a.xAxis.parent = t;
+    a.yAxis.parent = t;
+    a.zAxis.parent = t;
+  }
+}
+
+function applyAxisToLink(scene : BABYLON.Scene, robot : urdf.Robot, l : string) {
+  let r = robot.links.get(l);
+  if (r && r.visuals[0].transform) {
+    applyAxisToTransform(scene, r.visuals[0].transform);
+  }
+}
+
+function applyAxisToJoint(scene : BABYLON.Scene, robot : urdf.Robot, j : string) {
+  let r = robot.joints.get(j);
+  if (r && r.transform) {
+    applyAxisToTransform(scene, r.transform);
+  }
+}
+
+var createScene = async function () {
+  // This creates a basic Babylon Scene object (non-mesh)
+    // Create a default ground and skybox.
+    const environment = scene.createDefaultEnvironment({
+      createGround: true,
+      createSkybox: false,
+      enableGroundMirror: true,
+      groundMirrorSizeRatio: 0.15
+  });
+
+  scene.useRightHandedSystem = true;
+  scene.clearColor = BABYLON.Color4.FromColor3(BABYLON.Color3.Black());// TODO (polyhobbyist) Make this configurable
+
+  // This creates and positions a free camera (non-mesh)
+  var camera = new BABYLON.ArcRotateCamera("camera1", - Math.PI / 3, 5 * Math.PI / 12, 10, new BABYLON.Vector3(0, 2, 0), scene);
+  camera.wheelDeltaPercentage = 0.01;
+  camera.minZ = 0.1;
+
+  const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
+
+  // This attaches the camera to the canvas
+  camera.attachControl(canvas, true);
+
+  var groundMaterial = new Materials.GridMaterial("groundMaterial", scene);
+  groundMaterial.majorUnitFrequency = 5;
+  groundMaterial.minorUnitVisibility = 0.5;
+  groundMaterial.gridRatio = 2;
+  groundMaterial.opacity = 0.8;
+  groundMaterial.useMaxLine = true;
+  groundMaterial.lineColor = BABYLON.Color3.Green();  // TODO (polyhobbyist) Make this configurable
+
+  var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 50, height: 50}, scene);
+  ground.material = groundMaterial;
+
+
+  return scene;
+};
+
+async function applyURDF(urdfText) {
+  try {
+    scene = await createScene();
+
+    var robot = await urdf.deserializeUrdfToRobot(urdfText);
+    robot.create(scene);
+  } catch (err) {
+    vscode.postMessage({
+      command: "error",
+      text: err,
+    });
+  }
+
+  //applyAxisToTransform(scene, robot.transform);
+
+  //applyAxisToLink(scene, robot, "base_link");
+  //applyAxisToLink(scene, robot, "right_leg");
+
+  //applyAxisToJoint(scene, robot, "base_to_right_leg");
+
+}
+// Main function that gets executed once the webview DOM loads
+async function main() {
+
+  window.addEventListener('message', event => {
+    const message = event.data; // The JSON data our extension sent
+    switch (message.command) {
+        case 'urdf':
+        applyURDF(message.urdf);
+        break;
+        case 'previewFile':
+        vscode.setState({previewFile: message.previewFile});
+        break;
+    }
+  });    
+
+  engine.runRenderLoop(function () {
+    if (scene != null) {
+      scene.render();
+    }
+  });
+  
+  engine.resize();
+  
+  window.addEventListener("resize", function () {
+      engine.resize();
+  });  
+  
+  
+}
+
+  // Just like a regular webpage we need to wait for the webview
+  // DOM to load before we can reference any of the HTML elements
+  // or toolkit components
+  window.addEventListener("load", main);
+  
